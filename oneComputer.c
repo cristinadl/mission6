e502eff9da44d220df2084de39633a8931fba77a
NissimBet <nissimbet123@outlook.com>
2019-12-05 18:52:44 -0600

    refactor

--- oneComputer.c
+++ oneComputer.c
@@ -1,4 +1,4 @@
-// Nissim Betesh Hassine 
+// Nissim Betesh Hassine
 // A00821339
 // Cristina Nohemí De León Martínez
 // A01282017
@@ -6,7 +6,6 @@
 // Servicio que genera folder backup
 //
 //
-
 
 #include <math.h>
 #include <pthread.h>
@@ -26,139 +25,37 @@
 #include <syslog.h>
 #include <string.h>
 
-typedef long double ld;
-typedef long long ll;
 
-// Constantes del programa
-char* DIRECTORIO = "./client 4";
-char* BACKUP = "./BackUp/client 4";
+char *DIRECTORIO_ORIGINAL = "./client 4";
+char *DIRECTORIO_BACKUP = "./BackUp/client 4";
 
-void copyFile(char* name) {
-   char aname[200], bname[200], ch;
-   strcpy(aname, DIRECTORIO);
-   strcpy(bname, BACKUP);
-   strcat(aname, "/");
-   strcat(bname, "/");
-   strcat(aname, name);
-   strcat(bname, name);
+void copyFile(char *);
+void deleteFiles();
+void copyFiles();
+void mycallback(ConstFSEventStreamRef, 
+                void *, size_t , void *, 
+                const FSEventStreamEventFlags[],
+                const FSEventStreamEventId[]);
 
-   FILE *source, *target;
- 
-   source = fopen(aname, "r");
- 
-   if (source == NULL)
-   {
-      printf("Press any key to exit...\n");
-      exit(EXIT_FAILURE);
-   }
- 
-   target = fopen(bname, "w");
- 
-   if (target == NULL)
-   {
-      fclose(source);
-      printf("Press any key to exit...\n");
-      exit(EXIT_FAILURE);
-   }
- 
-   while ((ch = fgetc(source)) != EOF)
-      fputc(ch, target);
- 
-   fclose(source);
-   fclose(target);
- 
-   return ;
-}
+int main(int argc, char **argv) {
+    pid_t processId, sessionId;
 
-void deleteFiles() {
-    struct dirent *de;  // Pointer for directory entry 
-  
-    // opendir() returns a pointer of DIR type.  
-    DIR *dr = opendir(BACKUP); 
-  
-    if (dr == NULL)  // opendir returns NULL if couldn't open directory 
-    { 
-        printf("Could not open current directory" ); 
-        return; 
-    } 
-  
-    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html 
-    // for readdir() 
-    while ((de = readdir(dr)) != NULL) {
-	char* fl = de->d_name;
-	if(fl[0] != '.') {
-            //printf("%s\n", de->d_name); 
-	    char totnam[200];
-	    strcpy(totnam, BACKUP);
-	    strcat(totnam, "/");
-	    strcat(totnam, fl);
-	    remove(totnam);
-	}
-    }
-  
-    closedir(dr);     
-    return; 
-} 	
-
-void copyFiles() {
-    struct dirent *de;  // Pointer for directory entry 
-  
-    // opendir() returns a pointer of DIR type.  
-    DIR *dr = opendir(DIRECTORIO); 
-  
-    if (dr == NULL)  // opendir returns NULL if couldn't open directory 
-    { 
-        printf("Could not open current directory" ); 
-        return; 
-    } 
-  
-    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html 
-    // for readdir() 
-    while ((de = readdir(dr)) != NULL) {
-	char* fl = de->d_name;
-	if(fl[0] != '.') {
-            //printf("%s\n", de->d_name); 
-	    copyFile(fl);
-	}
-    }
-  
-    closedir(dr);     
-    return; 
-} 	
-
-void mycallback(
-    ConstFSEventStreamRef streamRef,
-    void *clientCallBackInfo,
-    size_t numEvents,
-    void *eventPaths,
-    const FSEventStreamEventFlags eventFlags[],
-    const FSEventStreamEventId eventIds[])
-{
-	deleteFiles();
-	copyFiles();
-}
-
-// Función principal del programa
-int main(int argc, char** argv) {
-  pid_t pid, sid;
-    
-    pid = fork();
-    if (pid < 0) {
+    processId = fork();
+    if (processId < 0) {
         exit(1);
     }
-  
-    if (pid > 0) {
+
+    if (processId > 0) {
         exit(1);
     }
 
     umask(0);
-            
-            
-    sid = setsid();
-    if (sid < 0) {
+
+    sessionId = setsid();
+    if (sessionId < 0) {
         exit(1);
     }
-    
+
     if ((chdir("/Users/cristinadeleon/Documents/TEC/Materias/PrograAvanzada/Missions/Mission6")) < 0) {
         exit(1);
     }
@@ -166,30 +63,125 @@ int main(int argc, char** argv) {
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
-    
-	srand(time(0));
-/* Define variables and create a CFArray object containing
-       CFString objects containing paths to watch.
-     */
-    CFStringRef mypath = CFSTR("./client 4");
-    CFArrayRef pathsToWatch = CFArrayCreate(NULL, (const void **)&mypath, 1, NULL);
-    void *callbackInfo = NULL; // could put stream-specific data here.
-    FSEventStreamRef stream;
-    CFAbsoluteTime latency = 0.3; /* Latency in seconds */
 
-    /* Create the stream, passing in a callback */
-    stream = FSEventStreamCreate(NULL,
-        &mycallback,
-        callbackInfo,
-        pathsToWatch,
-        kFSEventStreamEventIdSinceNow, /* Or a previous event ID */
-        latency,
-        kFSEventStreamCreateFlagNone /* Flags explained in reference */
+    srand(time(0));
+    
+    CFStringRef pathToClient = CFSTR("./client 4");
+    CFArrayRef clientDirectory = CFArrayCreate(NULL, (const void **)&pathToClient, 1, NULL);
+
+    void *callbackInfo = NULL; 
+    FSEventStreamRef stream;
+    CFAbsoluteTime timeBetweenChecks = 0.3; 
+
+    stream = FSEventStreamCreate(
+        NULL, &mycallback, callbackInfo,
+        clientDirectory,
+        kFSEventStreamEventIdSinceNow, 
+        timeBetweenChecks,
+        kFSEventStreamCreateFlagNone 
     );
-    /* Create the stream before calling this. */
-    FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(),         kCFRunLoopDefaultMode); 	
+
+    FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
     FSEventStreamStart(stream);
     CFRunLoopRun();
     exit(0);
 }
+
+
+void copyFile(char *name) {
+    char original[200], backup[200], ch;
+
+    strcpy(original, DIRECTORIO_ORIGINAL);
+    strcpy(backup, DIRECTORIO_BACKUP);
+    
+    strcat(original, "/");
+    strcat(backup, "/");
+    strcat(original, name);
+    strcat(backup, name);
+
+    FILE *originalFile, *backupFile;
+
+    originalFile = fopen(original, "r");
+
+    if (originalFile == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    backupFile = fopen(backup, "w");
+
+    if (backupFile == NULL) {
+        fclose(originalFile);
+        exit(EXIT_FAILURE);
+    }
+
+    while ((ch = fgetc(originalFile)) != EOF) {
+        fputc(ch, backupFile);
+    }
+
+    fclose(originalFile);
+    fclose(backupFile);
+
+    return;
+}
+
+void deleteFiles() {
+    struct dirent *directoryEntry; 
+    
+    DIR *backupDirectory = opendir(DIRECTORIO_BACKUP);
+
+    if (backupDirectory == NULL) {
+        printf("Could not open current directory");
+        return;
+    }
+
+    
+    
+    while ((directoryEntry = readdir(backupDirectory)) != NULL) {
+        char *filename = directoryEntry->d_name;
+        if (filename[0] != '.') {
+            char totnam[200];
+            strcpy(totnam, DIRECTORIO_BACKUP);
+            strcat(totnam, "/");
+            strcat(totnam, filename);
+            remove(totnam);
+        }
+    }
+
+    closedir(backupDirectory);
+    return;
+}
+
+void copyFiles() {
+    struct dirent *directoryEntry; 
+    
+    DIR *originalDir = opendir(DIRECTORIO_ORIGINAL);
+
+    if (originalDir == NULL) {
+        return;
+    }
+   
+    while ((directoryEntry = readdir(originalDir)) != NULL) {
+        char *filename = directoryEntry->d_name;
+        if (filename[0] != '.') {
+            copyFile(filename);
+        }
+    }
+
+    closedir(originalDir);
+    return;
+}
+
+void mycallback(
+    ConstFSEventStreamRef streamRef,
+    void *clientCallBackInfo,
+    size_t numEvents,
+    void *eventPaths,
+    const FSEventStreamEventFlags eventFlags[],
+    const FSEventStreamEventId eventIds[]) {
+        
+    deleteFiles();
+    copyFiles();
+}
+
+
 
